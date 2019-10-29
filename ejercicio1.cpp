e63a9dd6fa896fa663682e9f33a602d1f1ef04ea
Default user <as.garcia11@uniandes.edu.co>
2019-10-29 21:50:01 +0000

    segunda vez

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,2 +1,13 @@
-asdasdasdasd
-asdasdasd
+#include<iostream>
+using namespace std;
+int main()
+{
+    double a;
+    cout<<"Ingrese el radio del circulo ";
+    cin>>a;
+    cout<<"el perimetro es "<<2*3.1416*a<<endl;
+    cout<<"el area es "<<3.1416*a*a<<endl;
+    cout<<"el diametro es "<<2*a<<endl;
+        
+    return 0; 
+}
